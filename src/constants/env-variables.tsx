3d23dbb8c59c4ed4a321a642178d09a9c04252a3
pth-1641 <pth16042001@gmail.com>
2023-01-19 22:43:02 +0700

    Fix 403 error

--- src/constants/env-variables.tsx
+++ src/constants/env-variables.tsx
@@ -1,4 +1,5 @@
 const crawlBaseUrl = import.meta.env.VITE_CRAWL_BASE_URL;
 const proxyServer = import.meta.env.VITE_PROXY_SERVER;
+const imageProxyServer = import.meta.env.VITE_IMAGE_PROXY_SERVER;
 
-export { crawlBaseUrl, proxyServer };
+export { crawlBaseUrl, proxyServer, imageProxyServer };
--- src/pages/read/[id].tsx
+++ src/pages/read/[id].tsx
@@ -1,7 +1,5 @@
-import axios from 'axios';
-import { useEffect, useState } from 'preact/hooks';
 import { useParams } from 'react-router-dom';
-import { crawlBaseUrl } from '../../constants/env-variables';
+import { crawlBaseUrl, imageProxyServer } from '../../constants/env-variables';
 import { useStoryPics } from '../../hooks/useStoryPics';
 
 function ReadChapter() {
@@ -10,30 +8,17 @@ function ReadChapter() {
     `${crawlBaseUrl}/truyen-tranh/${name}/${chapter}/${slug}`
   );
 
-  useEffect(() => {
-    (async () => {
-      let headersList = {
-        Accept: '*/*',
-        Referer: crawlBaseUrl,
-      };
-
-      let reqOptions = {
-        url: 'https://corsproxy.io/?https://i333.ntcdntempv3.com/data/images/65000/875574/001.jpg',
-        method: 'GET',
-        headers: headersList,
-      };
-
-      let response = await axios.request(reqOptions);
-      console.log(response.data);
-    })();
-  }, []);
-
   return (
     <div className='flex flex-col items-center'>
-      {/* {images?.map((image: { imgSrc: string }) => (
-        <img src={image.imgSrc} alt='' draggable={false} />
-      ))} */}
-      <img src='' alt='' />
+      {images?.map(({ imgSrc }: { imgSrc: string }) => {
+        return (
+          <img
+            src={`${imageProxyServer}?file=${imgSrc}`}
+            alt=''
+            draggable={false}
+          />
+        );
+      })}
     </div>
   );
 }
